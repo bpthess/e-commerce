b034c4bae818dc930f2d2da2a207eb5be66636a7
tylee <bpthess@naver.com>
2022-08-30 13:53:58 +0900

    Build: heroku build Proceeding

--- app/backend/frontend/src/setupProxy.js
+++ app/backend/frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     // ("Access-Control-Allow-Origin", "*"),
-    createProxyMiddleware("/api/*", {
+    createProxyMiddleware("/api/products", {
       target: "http://localhost:8000/",
       secure: false,
       changeOrigin: true,
